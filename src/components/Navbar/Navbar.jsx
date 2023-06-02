3ae4e97205112792a5d849f0900ef969f040d297
YusufHasan94 <yhpolok@gmail.com>
2023-06-02 12:39:27 +0600

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-violet-500">
+        <div className="navbar fixed bg-violet-500 z-20 top-0 bg-opacity-20">
             <div className="navbar-start">
                 <Link to="/">
-                   <button className='btn bg-transparent text-white hover:bg-violet-400'> Popular Shows</button>
+                   <button className='btn bg-transparent text-white bg-violet-600 hover:bg-violet-400'> Popular Shows</button>
                 </Link>
             </div>
       </div>
